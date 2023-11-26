37cf0a9a832a4a797c77ecec0d6452014d668e42
sofianbenomar <sofianbenomar838@gmail.com>
2023-11-26 20:02:48 +0100

    league

--- app/src/main/java/com/example/test/ui/settings/Settings.kt
+++ app/src/main/java/com/example/test/ui/settings/Settings.kt
@@ -1,15 +1,8 @@
 package com.example.test.ui.settings
+
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -23,7 +16,7 @@ import androidx.compose.ui.unit.sp
 import com.example.test.R
 
 @Composable
-fun Settingslogo(){
+fun SettingsLogo() {
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -31,25 +24,24 @@ fun Settingslogo(){
             .background(Color(0xFFFFA000))
             .padding(16.dp),
         horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.baseline_settings_24),
+            contentDescription = "Settings",
+            modifier = Modifier
+                .size(52.dp)
+                .padding(horizontal = 16.dp)
         )
-    {
-        Image(painter = painterResource(id = R.drawable.baseline_settings_24), contentDescription = "Settings",
-        modifier = Modifier
-            .height(52.dp)
-            .width(52.dp)
-            .padding(horizontal = 16.dp)
-
+        Text(
+            text = "Settings",
+            color = Color.White,
+            fontSize = 20.sp
         )
-        Text(text = "Settings",
-            modifier = Modifier, color = Color.White, fontSize = 20.sp)
-
-
-
-
     }
 }
+
 @Composable
-fun Accountsetting() {
+fun AccountSetting() {
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -57,88 +49,16 @@ fun Accountsetting() {
         horizontalAlignment = Alignment.Start,
         verticalArrangement = Arrangement.Bottom,
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 12.dp, horizontal = 16.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(
-                text = "Account Settings",
-                modifier = Modifier, color = Color.White, fontSize = 20.sp
-            )
-
-
-        }
-
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 12.dp, horizontal = 16.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(
-                text = "Edit Profile",
-                modifier = Modifier, color = Color.White, fontSize = 20.sp
-            )
-            Image(
-                painter = painterResource(id = R.drawable.baseline_keyboard_arrow_right_24),
-                contentDescription = "Edit Profile",
-                modifier = Modifier
-                    .height(52.dp)
-                    .width(52.dp)
-                    .padding(horizontal = 16.dp)
-            )
-
-
-        }
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 12.dp, horizontal = 16.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(
-                text = "Change password",
-                modifier = Modifier, color = Color.White, fontSize = 20.sp
-            )
-            Image(
-                painter = painterResource(id = R.drawable.baseline_keyboard_arrow_right_24),
-                contentDescription = "Change password",
-                modifier = Modifier
-                    .height(52.dp)
-                    .width(52.dp)
-                    .padding(horizontal = 16.dp)
-            )
-
-
-        }
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 12.dp, horizontal = 16.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(
-                text = "Push notifications",
-                modifier = Modifier, color = Color.White, fontSize = 20.sp
-            )
-            Image(
-                painter = painterResource(id = R.drawable.baseline_toggle_off_24),
-                contentDescription = "Push notifications",
-                modifier = Modifier
-                    .height(52.dp)
-                    .width(52.dp)
-                    .padding(horizontal = 16.dp))
-
-
+        // Gentag dette mønster for hver Row i denne Column
+        SettingItem("Account Settings", R.drawable.baseline_keyboard_arrow_right_24)
+        SettingItem("Edit Profile", R.drawable.baseline_keyboard_arrow_right_24)
+        SettingItem("Change Password", R.drawable.baseline_keyboard_arrow_right_24)
+        SettingItem("Push Notifications", R.drawable.baseline_toggle_off_24)
     }
-    }
-
-
 }
+
 @Composable
-fun moresettings() {
+fun MoreSettings() {
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -146,107 +66,55 @@ fun moresettings() {
         horizontalAlignment = Alignment.Start,
         verticalArrangement = Arrangement.Bottom,
     ) {
-        Row (
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 12.dp, horizontal = 16.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ){
-            Text(text = "More",
-                modifier = Modifier, color = Color.White, fontSize = 20.sp
-            )
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 12.dp, horizontal = 16.dp),
-                verticalAlignment = Alignment.CenterVertically
+        // Gentag dette mønster for hver Row i denne Column
+        SettingItem("More", R.drawable.baseline_keyboard_arrow_right_24)
+        SettingItem("About Us", R.drawable.baseline_keyboard_arrow_right_24)
+        SettingItem("Privacy Policy", R.drawable.baseline_keyboard_arrow_right_24)
+        SettingItem("Terms and Conditions", R.drawable.baseline_keyboard_arrow_right_24)
+    }
+}
 
-            ) {
-                Text(text = "About us",
-                modifier = Modifier, color = Color.White, fontSize = 20.sp
-                )
-                Image(
-                    painter = painterResource(id = R.drawable.baseline_keyboard_arrow_right_24),
-                    contentDescription = "About us",
-                    modifier = Modifier
-                        .height(52.dp)
-                        .width(52.dp)
-                        .padding(horizontal = 16.dp))
-
-
-            }
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 12.dp, horizontal = 16.dp),
-                verticalAlignment = Alignment.CenterVertically
-
-            ) {
-                Text(text = "Privacy policy",
-                    modifier = Modifier, color = Color.White, fontSize = 20.sp
-                )
-                Image(
-                    painter = painterResource(id = R.drawable.baseline_keyboard_arrow_right_24),
-                    contentDescription = "Privacy policy",
-                    modifier = Modifier
-                        .height(52.dp)
-                        .width(52.dp)
-                        .padding(horizontal = 16.dp))
-
-
-            }
-                }
-        Row (modifier = Modifier
+@Composable
+fun SettingItem(text: String, iconId: Int) {
+    Row(
+        modifier = Modifier
             .fillMaxWidth()
             .padding(vertical = 12.dp, horizontal = 16.dp),
-            verticalAlignment = Alignment.CenterVertically
-
-        ) {
-            Text(text = "Terms and condition",
-                modifier = Modifier, color = Color.White, fontSize = 20.sp
-            )
-            Image(
-                painter = painterResource(id = R.drawable.baseline_keyboard_arrow_right_24),
-                contentDescription = "Terms and condition",
-                modifier = Modifier
-                    .height(52.dp)
-                    .width(52.dp)
-                    .padding(horizontal = 16.dp))
-
-
-        }
-            }
-        }
-
-
-
-
-
-
-
-
-
-
-
-@Preview
-@Composable
-fun Previewsettingspage() {
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .background(
-            brush = Brush.verticalGradient(
-                colors = listOf(
-                    Color(android.graphics.Color.parseColor("000000")),
-                    Color(android.graphics.Color.parseColor("FF9900"))
-
-                )
-            )
-        )
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Settingslogo()
-        Accountsetting()
-        moresettings()
-
+        Text(
+            text = text,
+            color = Color.White,
+            fontSize = 20.sp
+        )
+        Spacer(modifier = Modifier.weight(1f)) // Tilføj spacer for at skubbe ikonet til højre
+        Image(
+            painter = painterResource(id = iconId),
+            contentDescription = text,
+            modifier = Modifier
+                .size(52.dp)
+                .padding(horizontal = 16.dp)
+        )
     }
 }
 
-
+@Preview(showBackground = true)
+@Composable
+fun PreviewSettingsPage() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                Brush.verticalGradient(
+                    colors = listOf(
+                        Color.Black,
+                        Color(0xFFFF9900)
+                    )
+                )
+            )
+    ) {
+        SettingsLogo()
+        AccountSetting()
+        MoreSettings()
+    }
+}
